05a999f378cea9630ca5ea41da3b45f2fc907a31
Mehrdad Niasari <mehrdad.niasari@rbc.com>
2018-01-25 16:26:07 -0500

    returnig indices instead of numbers

--- src/main/cpp/twoSum.cpp
+++ src/main/cpp/twoSum.cpp
@@ -9,41 +9,45 @@ using namespace std;
 
 typedef pair<int,int> pii;
 
-int arr[MAX];
+int numbers[MAX];
 
 // O(nlogn)
 pii twoSumUsingTwoPointer(int target, int arrSize){
+	vector< pii > arr;
+	for (int i=0;i<arrSize;i++)
+		arr.push_back(pii(numbers[i], i+1));
+
 	//if array is not sorted
-	sort(arr, arr+arrSize);
+	sort(arr.begin(), arr.end());
 
 	//using two pointers to iterat
 	int l=0, r=arrSize-1;
 
 	while (l<r){
-		if (arr[l]+arr[r] == target) return pii(arr[l],arr[r]);
-		if (arr[l] + arr[r] > target) r--;
+		if (arr[l].first+arr[r].first == target) return pii(arr[l].second,arr[r].second);
+		if (arr[l].first + arr[r].first > target) r--;
 		else l++;
 	}
 
-	return pii(NA,NA);
+	return pii(-1,-1);
 }
 
 // O(n)
 pii twoSumUsingHashMap(int target, int arrSize){
-	unordered_map<int,int> mp;
+	unordered_map<int,vector<int>> mp;
 
 	for (int i=0;i<arrSize; i++)
-		mp[arr[i]]++;
+		mp[numbers[i]].push_back(i+1);
 
 	for (int i=0;i<arrSize;i++){
-		int comp = target - arr[i];
-		if (comp == arr[i] && mp[comp] > 1) //if target - arr[i] == arr[i], we should make sure we have more than one arr[i] 
-			return pii(comp, comp);
-		else if (comp != arr[i] && mp.find(comp)!=mp.end())// otherwise, just check if there is any target - arr[i]
-			return pii(arr[i], comp);
+		int comp = target - numbers[i];
+		if (comp == numbers[i] && mp[comp].size() > 1) //if target - arr[i] == arr[i], we should make sure we have more than one arr[i] 
+			return pii(mp[comp][0], mp[comp][1]);
+		else if (comp != numbers[i] && mp.find(comp)!=mp.end())// otherwise, just check if there is any target - arr[i]
+			return pii(i+1, mp[comp][0]);
 	}
 
-	return pii(NA,NA);
+	return pii(-1, -1);
 }
 
 
@@ -52,13 +56,14 @@ int main(){
 
 	while (cin >> arrSize){
 		for (int i=0; i< arrSize; i++)
-			cin >> arr[i];
+			cin >> numbers[i];
 		cin >> target;
 
-		pii ans = twoSumUsingHashMap(target, arrSize);
-		if (ans.first == NA)
-			cout << "not possible" << endl;
-		else
-			cout << ans.first << "," << ans.second << endl;
+		pii ansTwoPointer = twoSumUsingTwoPointer(target, arrSize);
+		pii ansHashMap = twoSumUsingHashMap(target, arrSize);
+		
+		// the answer of the two could be different since the answer to the problem is not unique!
+		cout << "Two pointer approach: "<< ansTwoPointer.first << "," << ansTwoPointer.second << endl;
+		cout << "Two hashmap approach: "<< ansHashMap.first << "," << ansHashMap.second << endl;
 	}
 }
